719de3ef8b878b4da3efef1904ae45eb8126660a
Fifer2 <fifer2@yandex.ru>
2019-12-23 21:43:32 +0700

    refactor: refactroing for load-more-button.js

--- src/components/load-more-button.js
+++ src/components/load-more-button.js
@@ -1,5 +1,27 @@
-export const createLoadMoreButtonTemplate = () => {
+import {createElement} from '../utils.js';
+
+const createLoadMoreButtonTemplate = () => {
   return (
     `<button class="load-more" type="button">load more</button>`
   );
 };
+
+export default class LoadMoreButton {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createLoadMoreButtonTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(createLoadMoreButtonTemplate());
+    }
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
