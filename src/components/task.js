a907d0ab800728d335afc00e289bf0e436f67f7a
Fifer2 <fifer2@yandex.ru>
2020-01-03 21:31:20 +0700

    refactor: rewrite other components with params in constructor

--- src/components/task.js
+++ src/components/task.js
@@ -1,5 +1,6 @@
 import {MONTH_NAMES} from '../const.js';
-import {formatTime, createElement} from '../utils.js';
+import {formatTime} from '../utils.js';
+import {AbstractComponent} from './abstract.js';
 
 
 const createHashtagsMarkup = (hashtags) => {
@@ -79,25 +80,13 @@ const createTaskTemplate = (task) => {
   );
 };
 
-export default class TaskComponent {
+export default class TaskComponent extends AbstractComponent {
   constructor(task) {
+    super();
     this._task = task;
-    this._element = null;
   }
 
   getTemplate() {
     return createTaskTemplate(this._task);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
