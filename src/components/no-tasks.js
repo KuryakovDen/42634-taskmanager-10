4abc1152cad53ea663624ca52773b6c2ed20acba
Fifer2 <fifer2@yandex.ru>
2020-01-03 19:50:29 +0700

    refactor: rewrite other components without params in constructor

--- src/components/no-tasks.js
+++ src/components/no-tasks.js
@@ -1,4 +1,4 @@
-import {createElement} from '../utils.js';
+import {AbstractComponent} from './abstract.js';
 
 const createNoTasksTemplate = () => {
   return (
@@ -7,24 +7,8 @@ const createNoTasksTemplate = () => {
     </p>`);
 };
 
-export default class NoTasks {
-  constructor() {
-    this._element = null;
-  }
-
+export default class NoTasks extends AbstractComponent {
   getTemplate() {
     return createNoTasksTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
