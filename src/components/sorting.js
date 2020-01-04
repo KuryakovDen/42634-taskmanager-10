1589f1d74f6c7a2fe6eaf9a1018583f1e25a1e28
Fifer2 <fifer2@yandex.ru>
2020-01-04 21:45:07 +0700

    refactor: refactoring for sorting component

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,15 +1,22 @@
 import {AbstractComponent} from './abstract.js';
+import {SortType} from '../const.js';
 
 export const createSortingTemplate = () => {
   return (
     `<div class="board__filter-list">
-      <a href="#" class="board__filter">SORT BY DEFAULT</a>
-      <a href="#" class="board__filter">SORT BY DATE up</a>
-      <a href="#" class="board__filter">SORT BY DATE down</a>
+      <a href="#" class="board__filter">${SortType.DEFAULT.toUpperCase()}</a>
+      <a href="#" class="board__filter">SORT BY DATE ${SortType.DATE_UP}</a>
+      <a href="#" class="board__filter">SORT BY DATE ${SortType.DATE_DOWN}</a>
     </div>`);
 };
 
 export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currentSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
     return createSortingTemplate();
   }
